98bc8d7afe9ff99a8eb00d39479e479ba4a59b31
AndreyVishnevskiy <andrey.vishnevskiy@betterme.world>
2022-07-19 19:21:46 +0300

    init commit

--- src/main/java/ArraySorting.java
+++ src/main/java/ArraySorting.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ArraySorting {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Длина масива: ");
+        int n = sc.nextInt();
+        int[] arr = arrayRandom(n);
+        System.out.print(Arrays.toString(arr));
+
+    }
+
+    static int[] arrayRandom(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random()* 10);
+        }
+
+        return arr;
+    }
 }

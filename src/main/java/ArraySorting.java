4a71c39797de1d683af465c4713fa9f8d7c9fa42
AndreyVishnevskiy <andrey.vishnevskiy@betterme.world>
2022-07-21 18:42:59 +0300

    Sort-fix

--- src/main/java/ArraySorting.java
+++ src/main/java/ArraySorting.java
@@ -8,25 +8,26 @@ public class ArraySorting {
         int n = sc.nextInt();
         int[] arr = arrayRandom(n);
         System.out.println("Сгенерированый масив: " + Arrays.toString(arr));
-        System.out.println("Выберете тип сортировки: \n 1 - Bubble Sort \n 2 - Selection Sort" );
-        int type = sc.nextInt();
-        System.out.println(sortSelection(n, arr, type));
+        System.out.print("Выберете тип сортировки(Bubble, Selection): ");
+        String type = sc.next();
+        System.out.println("2е самое большое число масива - " + sortSelection(arr, type));
     }
 
     static int[] arrayRandom(int n) {
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random()* 10);
+            arr[i] = (int) (Math.random()* 100);
         }
         return arr;
     }
 
-    static int bubbleSort(int[] arr, int n) {
+    static int bubbleSort(int[] arr) {
         boolean status = true;
         int tmp;
+        int i = 0;
         while (status) {
             status = false;
-            for (int i = 0; i < arr.length-1; i++) {
+            for (i = 0; i < arr.length-1; i++) {
                 if (arr[i] > arr[i+1]){
                     tmp = arr[i];
                     arr[i] = arr[i+1];
@@ -36,35 +37,33 @@ public class ArraySorting {
             }
         }
         System.out.println("Отсортерованый масив Bubble Sort: " + Arrays.toString(arr));
-        int result = arr[n-2];
-        return result;
+        return arr[i-1];
     }
 
-    static int selectionSort(int[] arr, int n) {
+    static int selectionSort(int[] arr) {
         int tmp;
-        for (int i = 0; i < arr.length-1; i++) {
+        int i = 0;
+        for (i = 0; i < arr.length-1; i++) {
             int min = i;
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i+1; j < arr.length; j++)
                 if (arr[j] < arr[min])
                     min = j;
                     tmp = arr[min];
                     arr[min] = arr[i];
                     arr[i] = tmp;
-            }
         }
         System.out.println("Отсортерованый масив Selection Sort: " + Arrays.toString(arr));
-        int result = arr[n-2];
-        return result;
+        return arr[i-1];
     }
 
-    static int sortSelection(int n, int[] arr, int type) {
+    static int sortSelection(int[] arr, String type) {
         int result = 0;
         switch (type) {
-            case 1:
-                result = bubbleSort(arr, n);
+            case "Bubble":
+                result = bubbleSort(arr);
                 break;
-            case 2:
-                result = selectionSort(arr, n);
+            case "Selection":
+                result = selectionSort(arr);
                 break;
             default:
                 System.out.println("Ошибка");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,39 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Длина масива: ");
+        int n = sc.nextInt();
+        int[] arr = arrayRandom(n);
+        System.out.println("Original Array:" + Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("Sorted Array:" + Arrays.toString(arr));
+        System.out.println("2е самое большое число масива - " + selectionSort(arr));
     }
 
+    static int[] arrayRandom(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10);
+        }
+        return arr;
+    }
+
+    static int selectionSort(int arr[]) {
+        int tmp;
+        int max = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            int min = i;
+            for (int j = i+1; j < arr.length; j++)
+                if (arr[j] < arr[min])
+                    min = j;
+                    tmp = arr[min];
+                    arr[min] = arr[i];
+                    arr[i] = tmp;
+                    max = arr[i];
+        }
+        return arr[max];
+    }
 }

028335cd2a7989b8f845ccc50609d11f2ae8d680
AndreyVishnevskiy <andrey.vishnevskiy@betterme.world>
2022-07-25 15:00:24 +0300

    fix-method

--- src/main/java/NumbersCheck.java
+++ src/main/java/NumbersCheck.java
@@ -5,22 +5,31 @@ public class NumbersCheck {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите число: ");
         int n = scan.nextInt();
-        checkNumber(n);
+        if (positiveNumber(n) == true)
+            primeNumber(n);
     }
 
-    static void checkNumber (int n) {
+    static boolean positiveNumber (int n) {
+        boolean status;
         if (n > 0) {
             System.out.println("Число положительное");
-            int i = 2;
-            int j = 0;
-            while (i * i <= n && j != 1) {
-                if (n % i == 0)
-                    j = 1;
-                i++;
-            }
-            String answer = (j == 1) ? "Число составное" : "Число простое";
-            System.out.println(answer);
-        } else
+            status = true;
+        } else {
             System.out.println("Число отрицательное");
+            status = false;
+        }
+        return status;
+    }
+
+    static void primeNumber (int n) {
+        int i = 2;
+        int j = 0;
+        while (i * i <= n && j != 1) {
+            if (n % i == 0)
+                j = 1;
+            i++;
+        }
+        String answer = (j == 1) ? "Число составное" : "Число простое";
+        System.out.println(answer);
     }
 }

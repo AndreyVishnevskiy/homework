c265d402b9cf495fcda7c2edde00cf6b3f36bf8a
AndreyVishnevskiy <andrey.vishnevskiy@betterme.world>
2022-08-03 11:59:27 +0300

    MyArea

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//        task1();
     }
+//    public static void task1() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Длина масива: ");
+//        int n = sc.nextInt();
+//        int[] arr = arrayRandom(n);
+//        System.out.println("Сгенерированый масив: " + Arrays.toString(arr));
+//        System.out.print("Выберете тип сортировки(Bubble, Selection): ");
+//        String type = sc.next();
+//        System.out.println("2е самое большое число масива - " + sortSelection(arr, type));
+//    }
 }
